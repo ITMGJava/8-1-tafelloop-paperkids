1c2803aafaed12d576fc127d8b3f909a99d2133a
Petra Manders <paperkids@hotmail.com>
2020-09-30 12:33:17 +0200

    de uitwerking tafels

--- src/TafelLoop.java
+++ src/TafelLoop.java
@@ -1,2 +1,31 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
 public class TafelLoop {
+    public static void main(String[] args) {
+        for (; ; ) {
+
+            System.out.println("voer het getal van de tafel in:");
+            var scanner1 = new Scanner(System.in);
+            var getal = scanner1.nextInt();
+
+
+            for (int i = 1; i <= 10; i++) {
+
+                System.out.println(i + " * " + getal + "= " + getal);
+
+
+            }
+
+        }
+    }
 }
+
+
+
+
+
+
+
+
